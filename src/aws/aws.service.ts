c212c3c81bbcf11c38c0e6609ee96e1836994f70
jiwon <usagi0012@naver.com>
2024-01-18 16:10:29 +0900

    연결 테스트

--- src/aws/aws.service.ts
+++ src/aws/aws.service.ts
@@ -5,17 +5,17 @@ import * as AWS from "aws-sdk";
 
 @Injectable()
 export class AwsService {
-    // constructor(private readonly configService: ConfigService) {}
-    // s3 = new AWS.S3();
-    // async fileupload(file: Express.Multer.File) {
-    //     const upload = await this.s3
-    //         .upload({
-    //             Bucket: this.configService.get<string>("S3BUCKET"),
-    //             Key: `${Date.now()}-${file.originalname}`,
-    //             Body: file.buffer,
-    //             ACL: "public-read",
-    //         })
-    //         .promise();
-    //     return upload.Location;
-    // }
+    constructor(private readonly configService: ConfigService) {}
+    s3 = new AWS.S3();
+    async fileupload(file: Express.Multer.File) {
+        const upload = await this.s3
+            .upload({
+                Bucket: this.configService.get<string>("S3BUCKET"),
+                Key: `${Date.now()}-${file.originalname}`,
+                Body: file.buffer,
+                ACL: "public-read",
+            })
+            .promise();
+        return upload.Location;
+    }
 }
