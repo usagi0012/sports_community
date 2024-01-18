cceadddc09623c0fa1188737ae79444ffabdeeaf
jiwon <usagi0012@naver.com>
2024-01-18 17:43:13 +0900

    크롤링

--- src/place/place.controller.ts
+++ src/place/place.controller.ts
@@ -1,7 +1,15 @@
-import { Controller } from "@nestjs/common";
+import { Controller, Get } from "@nestjs/common";
 import { PlaceService } from "./place.service";
+import { url } from "inspector";
 
 @Controller("place")
 export class PlaceController {
     constructor(private readonly placeService: PlaceService) {}
+
+    // @Get()
+    // getdata() {
+    //     return this.placeService.getHtml(
+    //         "https://shareit.kr/search/venue?showEventTypeHeader=true&eventTypes=31",
+    //     );
+    // }
 }
--- src/place/place.service.ts
+++ src/place/place.service.ts
@@ -1,9 +1,29 @@
 import { Injectable } from "@nestjs/common";
 import type { NextApiRequest, NextApiResponse } from "next";
-import cheerio, { Element } from "cheerio";
+import * as cheerio from "cheerio";
 // import { ContentType } from './type';
 import puppeteer from "puppeteer-core";
 import chromium from "@sparticuz/chromium-min";
+import axios from "axios";
+import { ContentType } from "aws-sdk/clients/cloudsearchdomain";
 
 @Injectable()
-export class PlaceService {}
+export class PlaceService {
+    // async getHtml(url: string) {
+    //     const html = await axios.get(url);
+    //     const $ = cheerio.load(html.data);
+    //     let content: ContentType[] = [];
+    //     const place = $(".SpaceItemstyle__Container-sc-1wddfv-5 kjjHma");
+    //     place.map((idx, el) => {
+    //         content[idx] = {
+    //             name: $(el)
+    //                 .find(".SpaceItemstyle__HostName-sc-1wddfv-7 cYSAQg")
+    //                 .text(),
+    //             location: $(el)
+    //                 .find("SpaceItemstyle__Address-sc-1wddfv-15 ijNkov")
+    //                 .text(),
+    //             // href: $(el).find("a:first-child").attr("href"),
+    //         };
+    //     });
+    // }
+}
