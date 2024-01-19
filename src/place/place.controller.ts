8650e12d2157841cf829d4e9333efc8e66b97c12
jiwon <usagi0012@naver.com>
2024-01-19 14:11:00 +0900

    크롤링

--- src/place/place.controller.ts
+++ src/place/place.controller.ts
@@ -1,6 +1,7 @@
-import { Controller, Get } from "@nestjs/common";
+import { Controller, Get, Post } from "@nestjs/common";
 import { PlaceService } from "./place.service";
 import { url } from "inspector";
+import axios from "axios";
 
 @Controller("place")
 export class PlaceController {
@@ -12,14 +13,25 @@ export class PlaceController {
     //         "https://shareit.kr/search/venue?showEventTypeHeader=true&eventTypes=31",
     //     );
     // }
-    @Get()
-    async crawl(): Promise<string[]> {
-        const url =
-            "https://shareit.kr/search/venue?keyword=&realTime=true&approve=true&region=-1&areas=&eventTypes=31&spaceTypes=&equipments=&minPrice=0&maxPrice=-1&minPeople=0&discount=0&page=1&sort=popularity";
-        const content = await this.placeService.crawlWebsite(url);
-        const extractedTexts =
-            await this.placeService.extractTextFromTag(content);
+    // @Get()
+    // async crawl(): Promise<string[]> {
+    //     const url =
+    //         "https://shareit.kr/search/venue?keyword=&realTime=true&approve=true&region=-1&areas=&eventTypes=31&spaceTypes=&equipments=&minPrice=0&maxPrice=-1&minPeople=0&discount=0&page=1&sort=popularity";
+    //     const content = await this.placeService.crawlWebsite(url);
+    //     const extractedTexts =
+    //         await this.placeService.extractTextFromTag(content);
 
-        return extractedTexts;
+    //     return extractedTexts;
+    // }
+
+    @Get()
+    getData() {
+        try {
+            return this.placeService.crawlSpaces(
+                "https://shareit-resource.shareit.kr/spaces?keyword&realTime=true&approve=true&region=-1&eventTypes=31&spaceThemeSeq&minPrice=0&maxPrice=-1&minPeople=0&minArea&maxArea&discount=0&page=1&sort=popularity&work=false&life=true&size=30&type=search",
+            );
+        } catch (error) {
+            return { error: error.message };
+        }
     }
 }
--- src/place/place.service.ts
+++ src/place/place.service.ts
@@ -10,6 +10,55 @@ import { ContentType } from "aws-sdk/clients/cloudsearchdomain";
 
 @Injectable()
 export class PlaceService {
+    // async getTitle(url: string): Promise<string> {
+    //     const response = await axios.get(url);
+    //     const $ = cheerio.load(response.data);
+    //     const title = $("title").text();
+    //     return title;
+    // }
+
+    async crawlSpaces(url: string) {
+        try {
+            const response = await axios.get(url, {
+                headers: { "Content-Type": "application/json;charset=UTF-8" },
+                // params: {
+                //     keyword: "",
+                //     realTime: true,
+                //     approve: true,
+                //     region: -1,
+                //     eventTypes: 31,
+                //     spaceThemeSeq: "",
+                //     minPrice: 0,
+                //     maxPrice: -1,
+                //     minPeople: 0,
+                //     minArea: "",
+                //     maxArea: "",
+                //     discount: 0,
+                //     page: 1,
+                //     sort: "popularity",
+                //     work: false,
+                //     life: true,
+                //     size: 2,
+                //     type: "search",
+                // },
+            });
+
+            const $ = cheerio.load("");
+            // const result = response.data.content.resources.map(
+            //     (resource: any) => {
+            //         return {
+            //             address: resource.address,
+            //             hostName: resource.host.name,
+            //             photoPath: resource.photos[0].file.resourcePath,
+            //         };
+            //     },
+            // );
+
+            // return result;
+        } catch (error) {
+            throw new Error(`Error in crawlSpaces: ${error.message}`);
+        }
+    }
     // async getHtml(url: string) {
     //     const html = await axios.get(url);
     //     const $ = cheerio.load(html.data);
@@ -28,55 +77,55 @@ export class PlaceService {
     //     });
     // }
 
-    async crawlWebsite(url: string): Promise<string> {
-        const browser = await puppeteer.launch({ headless: "new" });
-        const page = await browser.newPage();
-        // await page.setViewport({
-        //     width: 1920,
-        //     height: 1080,
-        // });
+    // async crawlWebsite(url: string): Promise<string> {
+    //     const browser = await puppeteer.launch({ headless: "new" });
+    //     const page = await browser.newPage();
+    //     // await page.setViewport({
+    //     //     width: 1920,
+    //     //     height: 1080,
+    //     // });
 
-        // 네트워크 요청이 완료될 때까지 기다림
-        await page.goto(url, { waitUntil: "networkidle2" });
-        await page.click(
-            ".IntroMobilestyle__LifeContainer-sc-15hynk6-6.eUoLDw",
-        );
+    //     // 네트워크 요청이 완료될 때까지 기다림
+    //     await page.goto(url, { waitUntil: "networkidle2" });
+    //     await page.click(
+    //         ".IntroMobilestyle__LifeContainer-sc-15hynk6-6.eUoLDw",
+    //     );
 
-        // await page.waitForSelector(
-        //     ".GnbPCstyle__CommonBtn-sc-3mhzew-2.GnbPCstyle__Btn-sc-3mhzew-5.jygHnB",
-        // );
+    //     // await page.waitForSelector(
+    //     //     ".GnbPCstyle__CommonBtn-sc-3mhzew-2.GnbPCstyle__Btn-sc-3mhzew-5.jygHnB",
+    //     // );
 
-        // await page.click(
-        //     ".GnbPCstyle__CommonBtn-sc-3mhzew-2.GnbPCstyle__Btn-sc-3mhzew-5.jygHnB",
-        // );
+    //     // await page.click(
+    //     //     ".GnbPCstyle__CommonBtn-sc-3mhzew-2.GnbPCstyle__Btn-sc-3mhzew-5.jygHnB",
+    //     // );
 
-        // await page.waitForSelector(
-        //     ".SpaceItemstyle__SpaceItem-sc-1wddfv-0.fQSBZt",
-        // );
+    //     // await page.waitForSelector(
+    //     //     ".SpaceItemstyle__SpaceItem-sc-1wddfv-0.fQSBZt",
+    //     // );
 
-        const content = await page.content();
-        await browser.close();
-        // console.log(content);
-        return content;
-    }
+    //     const content = await page.content();
+    //     await browser.close();
+    //     // console.log(content);
+    //     return content;
+    // }
 
-    async extractTextFromTag(content: string): Promise<string[]> {
-        const browser = await puppeteer.launch({ headless: false });
-        const page = await browser.newPage();
-        await page.setContent(content);
-        console.log(content);
-        const elements = await page.$$(
-            ".SpaceItemstyle__HostName-sc-1wddfv-7.cYSAQg",
-        );
-        console.log(elements);
-        const texts = await Promise.all(
-            elements.map((element) =>
-                page.evaluate((el) => el.textContent, element),
-            ),
-        );
+    // async extractTextFromTag(content: string): Promise<string[]> {
+    //     const browser = await puppeteer.launch({ headless: false });
+    //     const page = await browser.newPage();
+    //     await page.setContent(content);
+    //     console.log(content);
+    //     const elements = await page.$$(
+    //         ".SpaceItemstyle__HostName-sc-1wddfv-7.cYSAQg",
+    //     );
+    //     console.log(elements);
+    //     const texts = await Promise.all(
+    //         elements.map((element) =>
+    //             page.evaluate((el) => el.textContent, element),
+    //         ),
+    //     );
 
-        await browser.close();
+    //     await browser.close();
 
-        return texts;
-    }
+    //     return texts;
+    // }
 }
