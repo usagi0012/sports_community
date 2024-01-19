08bbde9681b394842351b7d960abf90fd4f50db9
jiwon <usagi0012@naver.com>
2024-01-19 10:39:05 +0900

    크롤링 페이지 로딩 불가

--- src/place/place.controller.ts
+++ src/place/place.controller.ts
@@ -12,4 +12,14 @@ export class PlaceController {
     //         "https://shareit.kr/search/venue?showEventTypeHeader=true&eventTypes=31",
     //     );
     // }
+    @Get()
+    async crawl(): Promise<string[]> {
+        const url =
+            "https://shareit.kr/search/venue?keyword=&realTime=true&approve=true&region=-1&areas=&eventTypes=31&spaceTypes=&equipments=&minPrice=0&maxPrice=-1&minPeople=0&discount=0&page=1&sort=popularity";
+        const content = await this.placeService.crawlWebsite(url);
+        const extractedTexts =
+            await this.placeService.extractTextFromTag(content);
+
+        return extractedTexts;
+    }
 }
--- src/place/place.service.ts
+++ src/place/place.service.ts
@@ -2,7 +2,8 @@ import { Injectable } from "@nestjs/common";
 import type { NextApiRequest, NextApiResponse } from "next";
 import * as cheerio from "cheerio";
 // import { ContentType } from './type';
-import puppeteer from "puppeteer-core";
+// import puppeteer from "puppeteer-core";
+import * as puppeteer from "puppeteer";
 import chromium from "@sparticuz/chromium-min";
 import axios from "axios";
 import { ContentType } from "aws-sdk/clients/cloudsearchdomain";
@@ -26,4 +27,56 @@ export class PlaceService {
     //         };
     //     });
     // }
+
+    async crawlWebsite(url: string): Promise<string> {
+        const browser = await puppeteer.launch({ headless: "new" });
+        const page = await browser.newPage();
+        // await page.setViewport({
+        //     width: 1920,
+        //     height: 1080,
+        // });
+
+        // 네트워크 요청이 완료될 때까지 기다림
+        await page.goto(url, { waitUntil: "networkidle2" });
+        await page.click(
+            ".IntroMobilestyle__LifeContainer-sc-15hynk6-6.eUoLDw",
+        );
+
+        // await page.waitForSelector(
+        //     ".GnbPCstyle__CommonBtn-sc-3mhzew-2.GnbPCstyle__Btn-sc-3mhzew-5.jygHnB",
+        // );
+
+        // await page.click(
+        //     ".GnbPCstyle__CommonBtn-sc-3mhzew-2.GnbPCstyle__Btn-sc-3mhzew-5.jygHnB",
+        // );
+
+        // await page.waitForSelector(
+        //     ".SpaceItemstyle__SpaceItem-sc-1wddfv-0.fQSBZt",
+        // );
+
+        const content = await page.content();
+        await browser.close();
+        // console.log(content);
+        return content;
+    }
+
+    async extractTextFromTag(content: string): Promise<string[]> {
+        const browser = await puppeteer.launch({ headless: false });
+        const page = await browser.newPage();
+        await page.setContent(content);
+        console.log(content);
+        const elements = await page.$$(
+            ".SpaceItemstyle__HostName-sc-1wddfv-7.cYSAQg",
+        );
+        console.log(elements);
+        const texts = await Promise.all(
+            elements.map((element) =>
+                page.evaluate((el) => el.textContent, element),
+            ),
+        );
+
+        await browser.close();
+
+        return texts;
+    }
 }
