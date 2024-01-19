28347f28d189555fa7adeea33c6e68f8a0c53e4f
jiwon <usagi0012@naver.com>
2024-01-19 14:55:22 +0900

    이미지 링크 생성

--- src/aws/aws.service.ts
+++ src/aws/aws.service.ts
@@ -8,16 +8,46 @@ export class AwsService {
     constructor(private readonly configService: ConfigService) {}
     s3 = new AWS.S3();
 
+    // async fileupload(file: Express.Multer.File) {
+    //     const upload = await this.s3
+    //         .upload({
+    //             Bucket: this.configService.get<string>("S3BUCKET"),
+    //             Key: `${Date.now()}-${file.originalname}`,
+    //             Body: file.buffer,
+    //             ACL: "public-read",
+    //         })
+    //         .promise();
+
+    //     return upload.Location;
+    // }
+
     async fileupload(file: Express.Multer.File) {
-        const upload = await this.s3
-            .upload({
+        const s3Client = new S3Client({
+            region: this.configService.get<string>("AWS_REGION"),
+            credentials: {
+                accessKeyId:
+                    this.configService.get<string>("AWS_ACCESS_KEY_ID"),
+                secretAccessKey: this.configService.get<string>(
+                    "AWS_SECRET_ACCESS_KEY",
+                ),
+            },
+        });
+        const objectKey = `${Date.now()}-${encodeURIComponent(
+            file.originalname,
+        )}`;
+        const upload = await s3Client.send(
+            new PutObjectCommand({
                 Bucket: this.configService.get<string>("S3BUCKET"),
-                Key: `${Date.now()}-${file.originalname}`,
+                Key: objectKey,
                 Body: file.buffer,
                 ACL: "public-read",
-            })
-            .promise();
+                ContentType: file.mimetype,
+            }),
+        );
 
-        return upload.Location;
+        const objectUrl = `https://${this.configService.get<string>(
+            "S3BUCKET",
+        )}.s3.amazonaws.com/${objectKey}`;
+        return objectUrl;
     }
 }
