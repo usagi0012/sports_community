4dca5bb9311e0f8eb8fad1ddd554c3362fea3885
IMCORIN2 <k_yoon1121@naver.com>
2024-01-22 11:05:30 +0900

    update:sweggar example 추가

--- src/applying-club/dto/permission-application.dto.ts
+++ src/applying-club/dto/permission-application.dto.ts
@@ -4,6 +4,6 @@ import { IsNotEmpty, IsNumber, IsString } from "class-validator";
 export class PermissionApplicationDto {
     @IsString()
     @IsNotEmpty({ message: "동호회에 보낼 메세지를 입력해주세요." })
-    @ApiProperty({ description: "메세지" })
+    @ApiProperty({ description: "메세지", example: "꼭 좀 가입시켜주세요." })
     message: string;
 }
