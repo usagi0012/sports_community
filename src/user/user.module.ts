aa296094e0fd62405bca72a49097267270933abe
unknown <rladydrms98@naver.com>
2024-01-26 11:10:35 +0900

    fix/ dev pull받기

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -5,10 +5,11 @@ import { TypeOrmModule } from "@nestjs/typeorm";
 import { User } from "../entity/user.entity";
 import { AuthModule } from "../auth/auth.module";
 import { ClubApplication } from "src/entity/club-application.entity";
-
+import { Report } from "src/entity/report.entity";
+import { Banlist } from "src/entity/banlist.entity";
 @Module({
     imports: [
-        TypeOrmModule.forFeature([User, ClubApplication]),
+        TypeOrmModule.forFeature([User, ClubApplication, Report, Banlist]),
         forwardRef(() => AuthModule),
     ],
     exports: [UserService, TypeOrmModule.forFeature([User, ClubApplication])],
