3452492aae2de564a0bfd123cb1e2da1cfcbbaf3
IMCORIN2 <k_yoon1121@naver.com>
2024-02-08 11:49:49 +0900

    feat/신청 취소 기능 구현

--- assets/js/myApplication.js
+++ assets/js/myApplication.js
@@ -12,6 +12,7 @@ function getMyApplication() {
             const mainContainer = document.querySelector(".mainContainer");
 
             const clubApplication = document.createElement("div");
+            clubApplication.className = "clubApplication";
             mainContainer.appendChild(clubApplication);
 
             // 동아리 id가 아니라 동아리 번호로 수정하기
@@ -29,9 +30,78 @@ function getMyApplication() {
             status.className = "status";
             status.innerHTML = `${response.data.data.status}`;
             clubApplication.appendChild(status);
+
+            // const modifyApplicationBtn = document.createElement("button");
+            // modifyApplicationBtn.innerHTML = "신청서 수정";
+            // clubApplication.appendChild(modifyApplicationBtn);
+
+            // modifyApplicationBtn.addEventListener("click", openModifyModal);
+
+            const deleteApplicationBtn = document.createElement("button");
+            deleteApplicationBtn.innerHTML = "신청 취소";
+            clubApplication.appendChild(deleteApplicationBtn);
+
+            deleteApplicationBtn.addEventListener(
+                "click",
+                confirmDeleteApplcation,
+            );
         })
         .catch(function (error) {
             console.log(error);
         });
 }
 getMyApplication();
+
+function openModifyModal() {
+    const modifyModal = document.querySelector("#modal");
+    modifyModal.style.display = "flex";
+}
+
+// function modifyApplication() {
+//     const token = localStorage.getItem("accessToken");
+//     const message = document.querySelector("#message").value;
+//     axios
+//         .put(
+//             `/api/applying-club/${clubId}`,
+//             { message },
+//             {
+//                 headers: {
+//                     Authorization: `Bearer ${token}`,
+//                 },
+//             },
+//         )
+//         .then(function (response) {
+//             console.log("==response==", response);
+//         })
+//         .catch(function (error) {
+//             console.log(error);
+//         });
+// }
+
+function deleteApplication() {
+    const accessToken = localStorage.getItem("accessToken");
+
+    axios
+        .delete(`/api/applying-club/`, {
+            headers: {
+                Authorization: `Bearer ${accessToken}`,
+            },
+        })
+        .then(function (response) {
+            alert("삭제가 완료되었습니다.");
+        })
+        .catch(function (error) {
+            alert("삭제에 실패했습니다.");
+        });
+}
+
+function confirmDeleteApplcation() {
+    let result = confirm("신청서를 정말로 삭제하시겠습니까?");
+    if (result) {
+        console.log("확인 누름");
+        deleteApplication();
+    } else {
+        console.log("취소 누름");
+        alert("신청서 삭제를 취소했습니다.");
+    }
+}
